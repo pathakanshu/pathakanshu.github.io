4acc46bf7441d9640d6476bdd1304845512dee4e
Anshu Pathak <anshuapr@gmail.com>
2026-02-11 22:06:41 -0500

    chore: created columns

--- tests/portfolio.spec.ts
+++ tests/portfolio.spec.ts
@@ -1,72 +1,74 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-if(!process.env.BASE_URL) {
-  // When not running on Github Actions, use 127.0.0.1
-  process.env.BASE_URL = "http://127.0.0.1:4321"
+if (!process.env.BASE_URL) {
+  // When not running on Github Actions, use localhost
+  process.env.BASE_URL = "http://localhost:4321";
 }
 
-test('has title', async ({ page }) => {
+test("has title", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
   await expect(page).not.toHaveTitle(/Astro/);
 });
 
-test('has h1', async ({page}) => {
+test("has h1", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  await expect(page.getByRole('heading', { name: 'Astro', level: 1 })).not.toBeVisible();
-  const h1 = page.locator('h1');
+  await expect(
+    page.getByRole("heading", { name: "Astro", level: 1 }),
+  ).not.toBeVisible();
+  const h1 = page.locator("h1");
   await expect(h1).not.toBeEmpty();
 });
 
-test('has nav', async ({page}) => {
+test("has nav", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  const nav = page.locator('nav');
+  const nav = page.locator("nav");
   await expect(nav).not.toBeEmpty();
 });
 
-test('has and follows links', async ({page}) => {
+test("has and follows links", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  await page.getByRole('link', { name: /home/i}).click();
-  await page.getByRole('link', { name: /about/i}).click();
+  await page.getByRole("link", { name: /home/i }).click();
+  await page.getByRole("link", { name: /about/i }).click();
 });
 
-test('has footer', async ({page}) => {
+test("has footer", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  const footer = page.locator('footer');
+  const footer = page.locator("footer");
   await expect(footer).not.toBeEmpty();
 });
 
-test('has at least one section', async ({page}) => {
+test("has at least one section", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  const section = page.locator('section');
+  const section = page.locator("section");
   await expect(section).not.toBeEmpty();
 });
 
-test('has at least one paragraph', async ({page}) => {
+test("has at least one paragraph", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  const para = page.locator('p');
+  const para = page.locator("p");
   await expect(para).not.toBeEmpty();
 });
 
-test('has a main element', async ({page}) => {
+test("has a main element", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  const main = page.locator('main');
+  const main = page.locator("main");
   await expect(main).not.toBeEmpty();
 });
 
-test('has an img element', async ({page}) => {
+test("has an img element", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  const img = page.getByRole('img');
+  const img = page.getByRole("img");
   await expect(img).not.toBeNull();
 });
 
-test('img element has src attribute', async ({page}) => {
+test("img element has src attribute", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  const img = page.locator('img');
-  await expect(img).toHaveAttribute('src');
+  const img = page.locator("img");
+  await expect(img).toHaveAttribute("src");
 });
 
-test('img element has alt attribute', async ({page}) => {
+test("img element has alt attribute", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  const img = page.locator('img');
-  await expect(img).toHaveAttribute('alt');
+  const img = page.locator("img");
+  await expect(img).toHaveAttribute("alt");
 });
