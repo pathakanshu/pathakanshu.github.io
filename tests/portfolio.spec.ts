e6d73c170c012d7c31f171f52290153418002fe5
Anshu Pathak <anshuapr@gmail.com>
2026-04-20 10:28:06 -0400

    fix: update tests to handle multiple 'about' links and fix strict mode violation

--- tests/portfolio.spec.ts
+++ tests/portfolio.spec.ts
@@ -1,34 +1,36 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-if(!process.env.BASE_URL) {
+if (!process.env.BASE_URL) {
   // When not running on Github Actions, use localhost
-  process.env.BASE_URL = "http://localhost:4321"
+  process.env.BASE_URL = "http://localhost:4321";
 }
 
-test('has title', async ({ page }) => {
+test("has title", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
   await expect(page).not.toHaveTitle(/Astro/);
 });
 
-test('has h1', async ({page}) => {
+test("has h1", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  await expect(page.getByRole('heading', { name: 'Astro', level: 1 })).not.toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Astro", level: 1 }),
+  ).not.toBeVisible();
 });
 
-test('has nav', async ({page}) => {
+test("has nav", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  const nav = page.locator('nav');
+  const nav = page.locator("nav");
   await expect(nav).toBeVisible();
 });
 
-test('has and follows links', async ({page}) => {
+test("has and follows links", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  await page.getByRole('link', { name: /home/i}).click();
-  await page.getByRole('link', { name: /about/i}).click();
+  await page.locator("nav").getByRole("link", { name: /home/i }).click();
+  await page.locator("nav").getByRole("link", { name: /about/i }).click();
 });
 
-test('has footer', async ({page}) => {
+test("has footer", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
-  const footer = page.locator('footer');
+  const footer = page.locator("footer");
   await expect(footer).toBeVisible();
 });
